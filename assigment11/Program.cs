04ebf949ee0c96e5258e883a2e8d8e5c3471cb4e
Dell <Dell@DESKTOP-49RPUBB>
2025-08-02 00:16:58 +0300

    Add project files.

---
+++ assigment11/Program.cs
@@ -0,0 +1,11 @@
+﻿
+
+int smallcarpet = 25;
+int largecarpet = 35;
+float tax = 0.06f;
+Console.WriteLine("enter the num of smallcarpet u wanna to wash");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("enter the num of  large carpet u wanna to wash");
+int num2 = Convert.ToInt32(Console.ReadLine());
+float total = smallcarpet * num1 + largecarpet * num2 + tax;
+Console.WriteLine($"total={total}");
